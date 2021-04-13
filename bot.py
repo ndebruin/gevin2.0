5ece24994564617abe33c1d90557021cdc690bee
ndebruin <ndebruin@draigon.org>
2021-04-13 13:50:04 -0400

    got pokemon thing working

--- bot.py
+++ bot.py
@@ -7,4 +7,15 @@ load_dotenv()
 
 client = discord.Client()
 
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if "Congratulations" in str(message.content) and str(message.author.id) == "716390085896962058":
+        await message.channel.send(message.author.mention+", That is the wrong Pokémon!")
+    
+client.run(getenv("KEY"))
