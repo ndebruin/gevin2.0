d54dcfe36a9167f80210bf678a648ccd60f037cc
ndebruin <ndebruin@draigon.org>
2021-04-13 14:09:52 -0400

    added dick

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@
 from os import getenv
 import discord
 from dotenv import load_dotenv
+from random import randint
 
 load_dotenv()
 
@@ -17,5 +18,13 @@ async def on_message(message):
         return
     if "Congratulations" in str(message.content) and str(message.author.id) == "716390085896962058":
         await message.channel.send(message.author.mention+", That is the wrong Pokémon!")
-    
+    if "cock" in str(message.content.lower()) or "dick" in str(message.content.lower()):
+        length = randint(0, 10)
+        strlen = ""
+        for i in range(length):
+            strlen += '='
+            i
+        await message.channel.send(message.author.mention+" Your dick is this long ˅```8" + strlen + "D```")
+
+
 client.run(getenv("KEY"))
