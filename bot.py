ce7afcc8864aeaf5e1a2f3f958249e4ded90886e
draigon <git@alias.draigon.org>
2021-04-18 11:59:11 -0400

    moved existing funcs to sep file, and completed ^what day

--- bot.py
+++ bot.py
@@ -3,68 +3,21 @@ from os import getenv
 import discord
 from dotenv import load_dotenv
 from random import randint
-import icalendar
-import recurring_ical_events
-from datetime import date
 import aiocron
 import asyncio
-from requests import get
+from calfunc import get_dailyevents, format_dailyevents
 
 load_dotenv()
 client = discord.Client()
 
-callink = getenv("LINK")
-
-def get_dailyevents():
-    cal = get(callink)
-    cal = cal.text
-    olddate = ""
-    today = date.today()
-
-    calendar = icalendar.Calendar.from_ical(cal)
-    events = recurring_ical_events.of(calendar).at(2021)
-
-    for event in events:
-            day = event["DTSTART"].dt
-            if str(day) == str(today):
-                    summary = event["SUMMARY"]
-                    if olddate == day and "asynchronous" in summary.lower():
-                            events.append("Asynchronous Day")
-                    else:
-                            events = [str(day), str(summary)]
-                    olddate = day
-            else:
-                return(1)    
-    return(events)
-
-def format_dailyevents():
-    dailyevents = get_dailyevents()
-    if dailyevents == 1:
-        return(1)
-    elif str(date.today()) == "2021-05-31":
-        return("@everyone Today is Memorial Day. Enjoy your day off of school!")
-    elif str(date.today()) == "2021-06-10":
-        return("@everyone Today is the **last** day of School! It is also a {}.".format(dailyevents[1]))
-    elif str(date.today()) == dailyevents[0]:
-        day = dailyevents[1]
-        if "Asynchronous Day" in dailyevents:
-            return("@everyone Today is a {}, and is an Asynchronous Day.".format(day))
-        else:
-            return("@everyone Today is a {}.".format(day))
-    else:
-        return(1)
-
-async def notify_dailyevents():
+@aiocron.crontab('0 7 * * 1-5')
+async def daily_notify():
     channel = client.get_channel(812364127439552563)
-    message = format_dailyevents()
+    message = "@everyone " + str(format_dailyevents())
     if message == 1:
         return(1)
     else:
         await channel.send(str(format_dailyevents()))
-
-@aiocron.crontab('0 7 * * 1-5')
-async def daily_notify():
-    await notify_dailyevents()
 
 @aiocron.crontab('47 9 * * *')
 async def temp():
@@ -116,9 +69,15 @@ async def on_message(message):
     if "boobs" in str(message.content.lower()) or "tits" in str(message.content.lower()) or "breasts" in str(message.content.lower()):
         await message.channel.send("``(.) (.)``")
     if "^what day" in str(message.content.lower()):
-        temp = await notify_dailyevents()
+        temp = format_dailyevents()
         if temp == 1:
             await message.channel.send(message.author.mention+" Today is not a school day.")
+        else:
+            await message.channel.send(message.author.mention + temp)
+#    if "^tomorrow" in str(message.content.lower()):
+#        temp = format_dailyevents()
+#        if temp == 1:
+#            await message.channel.send(message.author.mention+" Tomorrow is not a school day.")
         
 daily_notify.start()
 temp.start()
--- calfunc.py
+++ calfunc.py
@@ -2,10 +2,11 @@ import icalendar
 import recurring_ical_events
 from requests import get
 from os import getenv
-from datetime import date
+from datetime import date, timedelta
+from dotenv import load_dotenv
 
+load_dotenv()
 callink = getenv("LINK")
-
 
 def get_dailyevents():
     cal = get(callink)
@@ -29,3 +30,19 @@ def get_dailyevents():
                 return(1)    
     return(events)
 
+def format_dailyevents():
+    dailyevents = get_dailyevents()
+    if dailyevents == 1:
+        return(1)
+    elif str(date.today()) == "2021-05-31":
+        return("Today is Memorial Day. Enjoy your day off of school!")
+    elif str(date.today()) == "2021-06-10":
+        return("Today is the **last** day of School! It is also a {}.".format(dailyevents[1]))
+    elif str(date.today()) == dailyevents[0]:
+        day = dailyevents[1]
+        if "Asynchronous Day" in dailyevents:
+            return("Today is a {}, and is an Asynchronous Day.".format(day))
+        else:
+            return("Today is a {}.".format(day))
+    else:
+        return(1)
